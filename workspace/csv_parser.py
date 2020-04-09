25393457a49b36dcc7698bafc26caa99b6145310
Vivek Bhadra <vivek.bhadra@gmail.com>
2020-04-09 18:28:35 +0100

    Open Dialog.

--- workspace/csv_parser.py
+++ workspace/csv_parser.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QFileDialog
 
 class GUI(QMainWindow):
     def __init__(self):
@@ -8,14 +8,24 @@ class GUI(QMainWindow):
         self.initUI()
         
     def initUI(self):
-        self.setWindowTitle("Hello PyQt")
+        self.setWindowTitle("Window")
         self.resize(400, 600)
-            
+        self.move(10, 20)
+        
+        self.openFileNameDialog()
+        self.show()
+        
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","CSV Files (*.csv)", options=options)
+        
+        if fileName:
+            print(fileName)
+                    
 if __name__ == "__main__": 
     app = QApplication(sys.argv)
     gui = GUI()
-
-    #win.resize(400, 600)
-
-    gui.show()
+    
+    #
     sys.exit(app.exec_())
